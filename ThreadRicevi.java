5b53242ed84c0495e33b3558168b31739a9e86f2
mazz <lory.mazzoli0906@gmail.com>
2025-01-28 15:10:23 +0100

    primo

---
+++ ThreadRicevi.java
@@ -0,0 +1,30 @@
+package clientChat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ThreadRicevi implements Runnable{
+	private Socket socket;
+	 BufferedReader in; 
+	public  ThreadRicevi(Socket socket) throws IOException {
+		this.socket=socket;
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	} 
+	public void run() {
+		String messaggio;
+		try {
+			messaggio = in.readLine();
+		
+			while(messaggio!=null){
+				System.out.println(messaggio);
+				messaggio = in.readLine();
+			}
+			System.out.println("Server Chiuso");
+			socket.close();
+		} catch (IOException e) {
+			System.out.println("Errore di connessione");
+		}
+	}
+}
